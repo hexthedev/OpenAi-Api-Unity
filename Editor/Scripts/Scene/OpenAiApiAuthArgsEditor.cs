f6291e233357ba3e7a440301e72b345ac24db4d3
james <jamesmccafferty@live.ca>
2020-12-28 22:30:59 -0500

    Authentication methods need a little bit of work

---
+++ Editor/Scripts/Scene/OpenAiApiAuthArgsEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+
+namespace OpenAi.Api.V1
+{
+    [CustomEditor(typeof(OpenAiApiAuthArgs))]
+    public class OpenAiApiAuthArgsEditor : Editor
+    {
+        SerializedProperty AuthType;
+        SerializedProperty PrivateApiKey;
+
+        void OnEnable()
+        {
+            AuthType = serializedObject.FindProperty("AuthType");
+            PrivateApiKey = serializedObject.FindProperty("PrivateApiKey");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(AuthType);
+
+            switch((EAuthType)AuthType.enumValueIndex)
+            {
+                case EAuthType.LocalFile:
+                    EditorGUILayout.HelpBox("This auth method will attempt to find the private key at `~/.openai/key.txt` (Linux/Mac) or `%USERPROFILE%/.openai/key.txt` (Windows). If this file does not exist or the key is not present, api calls will fail", MessageType.Warning);
+                    break;
+                case EAuthType.String:
+                    EditorGUILayout.PropertyField(PrivateApiKey);
+                    break;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- Runtime/Scripts/Scene/OpenAiApiAuthArgs.cs
+++ Runtime/Scripts/Scene/OpenAiApiAuthArgs.cs
@@ -5,9 +5,14 @@ namespace OpenAi.Api.V1
     /// <summary>
     /// The Authentication arguments required to authenticate an OpenAI Api request. This file should not be populated and exposed to the public, as the private key secret, and revealed to the public breaches the OpenAi terms and conditions. 
     /// </summary>
-    [CreateAssetMenu(fileName = "OpenAi.ApiAuthArgs", menuName = "OpenAi/Api/AuthArgs")]
+    [CreateAssetMenu(fileName = "AuthArgs", menuName = "OpenAi/Api/AuthArgs")]
     public class OpenAiApiAuthArgs : ScriptableObject
     {
+        /// <summary>
+        /// The method by which the PrivateApiKey is found
+        /// </summary>
+        public EAuthType AuthType = EAuthType.LocalFile;
+
         /// <summary>
         /// The private key provided by OpenAi. You private api key can be found at <see href="https://beta.openai.com/docs/developer-quickstart/your-api-keys"/> if you have an account.
         /// </summary>
