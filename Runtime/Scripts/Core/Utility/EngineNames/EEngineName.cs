71df6da2832a0a535566ee75c6df4036c4ac57c8
james <jamesmccafferty@live.ca>
2021-01-06 14:28:45 -0500

    Adding enums to represent engine names

---
+++ Runtime/Scripts/Core/Utility/EngineNames/EEngineName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OpenAi.Api
+{
+    public enum EEngineName
+    {
+        ada,
+        babbage,
+        content_filter_alpha_c4,
+        content_filter_dev,
+        curie,
+        cursing_filter_v6,
+        davinci,
+        instruct_curie_beta,
+        instruct_davinci_beta
+    }
+
+    public static class UTEEngineName
+    {
+        public static string GetEngineName(EEngineName name)
+        {
+            switch (name)
+            {
+                case EEngineName.ada:
+                    return UTEngineNames.ada;
+                case EEngineName.babbage:
+                    return UTEngineNames.babbage;
+                case EEngineName.content_filter_alpha_c4:
+                    return UTEngineNames.content_filter_alpha_c4;
+                case EEngineName.content_filter_dev:
+                    return UTEngineNames.content_filter_dev;
+                case EEngineName.curie:
+                    return UTEngineNames.curie;
+                case EEngineName.cursing_filter_v6:
+                    return UTEngineNames.cursing_filter_v6;
+                case EEngineName.davinci:
+                    return UTEngineNames.davinci;
+                case EEngineName.instruct_curie_beta:
+                    return UTEngineNames.instruct_curie_beta;
+                case EEngineName.instruct_davinci_beta:
+                    return UTEngineNames.instruct_davinci_beta;
+            }
+
+            throw new ArgumentException($"Invalid enum value provided when getting engine name. Value provided: {name}");
+        }
+    }
+}
---
+++ Runtime/Scripts/Core/Utility/EngineNames/UTEngineNames.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenAi.Api
+{
+    public static class UTEngineNames
+    {
+        public const string ada = "ada";
+        public const string babbage = "babbage";
+        public const string content_filter_alpha_c4 = "content-filter-alpha-c4";
+        public const string content_filter_dev = "content-filter-dev";
+        public const string curie = "curie";
+        public const string cursing_filter_v6 = "cursing-filter-v6";
+        public const string davinci = "davinci";
+        public const string instruct_curie_beta = "instruct-curie-beta";
+        public const string instruct_davinci_beta = "instruct-davinci-beta";
+    }
+}
