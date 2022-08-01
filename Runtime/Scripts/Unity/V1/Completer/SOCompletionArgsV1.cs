b77d5a728be283814786ed1577c821f0b2835ddf
BenKoder <32285487+BenKoder@users.noreply.github.com>
2022-08-01 22:33:21 +1000

    Fix 400 bad request
    Hardcode stop to fix 400 bad request.

--- Runtime/Scripts/Unity/V1/Completer/SOCompletionArgsV1.cs
+++ Runtime/Scripts/Unity/V1/Completer/SOCompletionArgsV1.cs
@@ -35,7 +35,7 @@ namespace OpenAi.Unity.V1
                 max_tokens = max_tokens,
                 temperature = temperature,
                 top_p = top_p,
-                stop = stop,
+                stop = "\n",
                 frequency_penalty = frequency_penalty
             };
         }
